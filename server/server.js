0e4aec1c0b5f418ac9e841d057874d08c7f0f905
Matt Apperson <me@mattapperson.com>
2011-12-19 13:07:45 -0500

    Added new UI

--- server/server.js
+++ server/server.js
@@ -5,6 +5,20 @@ var Stack = require('stack'),
     sys = require('sys'),
 	exec = require('child_process').exec;
 	
+	
+var child;
+
+setInterval(function() {
+	// executes `pwd`
+	child = exec("git pull", function (error, stdout, stderr) {
+	  console.log('stdout: ' + stdout);
+	  console.log('stderr: ' + stderr);
+	  if (error !== null) {
+	    console.log('exec error: ' + error);
+	  }
+	});
+}, 1800000);
+
 Http.createServer(Stack(
   Creationix.log(),
   require('wheat')(process.env.JOYENT ? process.env.HOME + "/howtonode" : __dirname +"/..")
