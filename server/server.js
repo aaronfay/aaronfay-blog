b9726a600db8babb798d7648dc7f5e8f679ddba7
Aaron Fay <afay@strathcom.com>
2012-08-06 13:24:08 -0600

    added a little script to pull repo, test again

--- server/server.js
+++ server/server.js
@@ -2,25 +2,39 @@
 var Stack = require('stack'),
     Creationix = require('creationix'),
     Http = require('http'),
-    //sys = require('sys'),
-	exec = require('child_process').exec;
-	
-	
+    fs = require('fs'),
+  exec = require('child_process').exec;
+  
+  
 var child;
 
 setInterval(function() {
-	// executes `pwd`
-	child = exec("git pull", function (error, stdout, stderr) {
-	  console.log('stdout: ' + stdout);
-	  console.log('stderr: ' + stderr);
-	  if (error !== null) {
-	    console.log('exec error: ' + error);
-	  }
-	});
+  // executes `pwd`
+  child = exec("git pull", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
 }, 1800000);
 
-Http.createServer(Stack(
-  Creationix.log(),
-  require('wheat')(process.env.JOYENT ? process.env.HOME + "/howtonode" : __dirname +"/..")
-)).listen(process.env.PORT || 5000);
+var init = exec("git init", function (error, stdout, stderr) {
+  if (error !== null) {
+    console.log('git init error: ' + error);
+  } else {
+    // now pull the repo
+    var pull = exec("git pull https://github.com/aaronfay/aaronfay-blog.git", function (error, stdout, stderr) {
+      console.log('pulled .git repo');
+      runServer();
+    });
+  }
+});
+
+function runServer() {
+  Http.createServer(Stack(
+    Creationix.log(),
+    require('wheat')(process.env.JOYENT ? process.env.HOME + "/howtonode" : __dirname +"/..")
+  )).listen(process.env.PORT || 5000);
+}
 
